94b2afcdfe949e5fd36b1bddf85dce570e492248
souenxx <souen0823@gmail.com>
2021-10-10 22:32:45 +0900

    feat: posts page

--- project-backend/db/schema.rb
+++ project-backend/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_10_120959) do
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
