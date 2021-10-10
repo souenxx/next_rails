96af28d11b86fe59e01149fe4ce7a29d5251d13f
souenxx <souen0823@gmail.com>
2021-10-10 22:33:23 +0900

    chore: integration nextjs and rails

--- project-backend/config/environments/development.rb
+++ project-backend/config/environments/development.rb
@@ -63,4 +63,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #Hostname
+  Rails.application.config.hosts << 'localhost'
+  Rails.application.config.hosts << '127.0.0.1'
+  Rails.application.config.hosts << ENV['BACKEND_HOST']
 end
