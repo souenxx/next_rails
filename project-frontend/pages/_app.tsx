e9091d4475005cec34718d2863be40a3522db43f
souenxx <souen0823@gmail.com>
2021-10-26 22:42:44 +0900

    feat: apollo client

--- project-frontend/pages/_app.tsx
+++ project-frontend/pages/_app.tsx
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ApolloClient,InMemoryCache,ApolloProvider } from '@apollo/client'
+import { AppProps } from 'next/dist/shared/lib/router/router';
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
+  cache,
+  // headers:{
+  //   "Access-Control-Allow-Origin": ,
+  //   "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+  //   "Access-Control-Allow-Headers":
+  //     "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+  // },
+  credentials: 'omit'
+});
+
+function MyApp({ Component, pageProps }:AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
